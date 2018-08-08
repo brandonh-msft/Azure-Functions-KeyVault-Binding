cca2cad041a59de415023a4174922beb8f1c7e9f
Brandon Hurlburt <hurlburb@microsoft.com>
2018-08-08 10:45:15 -0700

    adding comments on the converter code for education

--- InputBinding/KeyVaultSecretAttribute.cs
+++ InputBinding/KeyVaultSecretAttribute.cs
@@ -5,6 +5,7 @@ namespace Functions.Extensions.KeyVault
 {
     /// <summary></summary>
     /// <seealso cref="System.Attribute" />
+    // denote the attribute is a function binding with [Binding]
     [Binding, AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)]
     public class KeyVaultSecretAttribute : Attribute
     {
@@ -14,6 +15,7 @@ namespace Functions.Extensions.KeyVault
         /// <param name="resourceNameSetting">The name of the application setting (eg: MyKeyVault) which holds the name of the Key Vault resource in Azure (eg: my-key-vault)</param>
         /// <param name="secretIdSetting">The name of the application setting which holds the id of the secret whose value should be fetched.</param>
         /// <remarks>You must add the appropriate access policies to your KeyVault instance for your Function App's Managed Service Identity in order for this binding to work.</remarks>
+        // I prefer to make *required* inputs to the binding be constructor parameters, that way users can't skip them
         public KeyVaultSecretAttribute(string resourceNameSetting, string secretIdSetting)
         {
             this.ResourceNameSetting = resourceNameSetting;
@@ -21,7 +23,8 @@ namespace Functions.Extensions.KeyVault
         }
 
         /// <summary>
-        /// Gets the name of the application setting (eg: MyKeyVault) which holds the name of the Key Vault resource in Azure (eg: my-key-vault)
+        /// Gets the name of the application setting (eg: MyKeyVault) which holds the name of the Key Vault resource in Azure (eg: my-key-vault).
+        /// Default: "KeyVaultResourceName"
         /// </summary>
         [AppSetting(Default = @"KeyVaultResourceName")]
         public string ResourceNameSetting { get; }
--- InputBinding/KeyVaultSecretConfiguration.cs
+++ InputBinding/KeyVaultSecretConfiguration.cs
@@ -19,8 +19,11 @@ namespace Functions.Extensions.KeyVault
         /// <param name="context">The context.</param>
         public void Initialize(ExtensionConfigContext context)
         {
+            // Tell the Functions host that we want to add a new binding based on the KeyVaultSecret attribute class
             context.AddBindingRule<KeyVaultSecretAttribute>()
+                // Let funchost know it's an Input binding, and how to convert it to the type the user specifies (eg: string). If you want the user to be able to use other types, you must add more 'BindToInput' calls that return those types as values. Here, I have to use a class implementing the IAsyncConverter because I need to call async methods to perform the conversion
                 .BindToInput(KeyVaultSecretConverter.Instance)
+                // Add a validator on the user's attribute implementation to make sure I can even do the conversion and blow up accordingly if I can't.
                 .AddValidator((attrib, t) =>
                 {
                     if (string.IsNullOrWhiteSpace(attrib.ResourceNameSetting))
@@ -40,13 +43,16 @@ namespace Functions.Extensions.KeyVault
         {
             private KeyVaultSecretConverter() { }
 
+            // Provide a static instance to the keyvault converter so the funchost doesn't have to spin it up over and over, potentially exhausting connections or getting rate-limited
             public static KeyVaultSecretConverter Instance { get; } = new KeyVaultSecretConverter();
 
+            // Similar to above; make these static, particularly the HttpClient, so as not to exhaust the connection pool
             private static readonly HttpClient _httpClient = new HttpClient();
             private static readonly AzureServiceTokenProvider _tokenProvider = new AzureServiceTokenProvider();
 
             private static readonly KeyVaultClient _kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(_tokenProvider.KeyVaultTokenCallback), _httpClient);
 
+            // "convert" means "take the attribute, and give me back the <T> (in this case string) the user's asking for." So here, it means "go hit the keyvault instance they've specified and get the value for the secret"
             public async Task<string> ConvertAsync(KeyVaultSecretAttribute attrib, CancellationToken cancellationToken)
             {
                 var secretBundle = await _kvClient.GetSecretAsync($@"https://{attrib.ResourceNameSetting}.vault.azure.net/secrets/{attrib.SecretIdSetting}");
